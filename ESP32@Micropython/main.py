52b2e6a13afee8dc6f4ca074cd1961ef34a94331
RickLiu-MBP <mushiking02134tc@gmail.com>
2024-08-03 20:10:50 +0800

    #Update: Test 10 frame pass, but speed is not fast

--- ESP32@Micropython/main.py
+++ ESP32@Micropython/main.py
@@ -9,7 +9,7 @@ cs = machine.Pin(15, machine.Pin.OUT)
 
 # Initialize SPI peripheral (start with 1 MHz)
 spi_sd = machine.SPI(1,
-                     baudrate=1000000,
+                     baudrate=20_000_000,
                      polarity=0,
                      phase=0,
                      bits=8,
@@ -19,7 +19,7 @@ spi_sd = machine.SPI(1,
                      miso=machine.Pin(12))
 
 spi_lcd = machine.SPI(2,
-                      baudrate=1000000,
+                      baudrate=20_000_000,
                       polarity=0,
                       phase=0,
                       bits=8,
@@ -34,6 +34,8 @@ class LCD12864(object):
         self.spi = lcd_spi
         self.rst = Pin(reset_pin, Pin.OUT, Pin.PULL_DOWN)
 
+        self.init()
+
     def send_bytes(self, comm_list):
         self.spi.write(bytearray(comm_list))
 
@@ -45,7 +47,7 @@ class LCD12864(object):
         ]
         self.send_bytes(_comm_list)
 
-    def send_char(self, char_hex):
+    def print_char(self, char_hex):
         _comm_list = [
             0xFA,
             char_hex & 0xF0,
@@ -55,9 +57,9 @@ class LCD12864(object):
 
     def print_line(self, msg):
         for c in msg:
-            self.send_char(ord(c))
+            self.print_char(ord(c))
 
-    def lcd_init(self):
+    def init(self):
         # Toggle reset pin
         self.rst.value(0)
         time.sleep_ms(50)
@@ -70,15 +72,39 @@ class LCD12864(object):
         self.send_command(0x01)
         self.send_command(0x06)
 
-    def lcd_on(self):
+    def on(self):
         self.send_command(0x0C)
 
-    def lcd_clear(self):
+    def clean(self):
         self.send_command(0x30)
         self.send_command(0x01)
+        time.sleep_ms(1)
 
     def cursor_home(self):
         self.send_command(0x02)
+
+    def draw(self, pixel_arr):
+        for y_line in range(64):
+            if y_line < 32:
+                x = 0x80
+                y = y_line + 0x80
+            else:
+                x = 0x88
+                y = y_line - 32 + 0x80
+
+            self.send_command(0x34)
+            self.send_command(y)
+            self.send_command(x)
+            self.send_command(0x30)
+
+            tmp = y_line * 16
+
+            for i in range(16):
+                pixel_val = pixel_arr[tmp + i]
+                self.print_char(pixel_val)
+
+            self.send_command(0x34)
+            self.send_command(0x36)
 
 
 if __name__ == '__main__':
@@ -94,16 +120,21 @@ if __name__ == '__main__':
     uos.mount(vfs, "/sd")
     # print(uos.listdir("/sd"))
 
-    # for idx in range(10):
-    #     file_name = f"f{idx:03d}"
-    #     f = open("/sd/" + file_name, 'r')
-    #     data_str = f.readline()[1:-1]
-    #     data_tuple = eval(data_str)
-    #     print(data_tuple)
-    #     f.close()
-
     lcd = LCD12864(spi_lcd, 22)
-    lcd.lcd_init()
     lcd.cursor_home()
     lcd.print_line("Hello World!!")
+
+    time.sleep(1)
+    _tuples = []
+    for idx in range(10):
+        file_name = f"f{idx:03d}"
+        f = open("/sd/" + file_name, 'r')
+        data_str = f.readline()[1:-1]
+        _tuples.append(eval(data_str))
+        f.close()
+
+    for _tup in _tuples:
+        lcd.clean()
+        lcd.draw(_tup)
+        time.sleep_ms(50)
 
