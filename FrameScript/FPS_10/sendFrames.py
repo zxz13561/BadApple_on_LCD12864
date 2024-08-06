f457ecf8772b085e913d364056fa8186939648f4
PG-Rick <rickliu@pg-tw.com>
2024-08-06 16:36:54 +0800

    #Update: Limit test on STM32H753, and update result

--- FrameScript/FPS_10/sendFrames.py
+++ FrameScript/FPS_10/sendFrames.py
@@ -1,4 +1,4 @@
-import os
+import time
 from time import perf_counter
 import serial
 from datetime import datetime
@@ -18,7 +18,7 @@ board_type = board_list[1]
 # Init Serial
 # port = "/dev/tty.usbserial-1120"
 port = "COM3"
-ser = serial.Serial(port, baudrate=230400, bytesize=8, parity=serial.PARITY_NONE, stopbits=1)
+ser = serial.Serial(port, baudrate=460800, bytesize=8, parity=serial.PARITY_NONE, stopbits=1)
 
 # Setup index range
 f_idx = 0
@@ -36,8 +36,10 @@ while True:
     # Receive REQ message from board
     recv = b''
     if ser.in_waiting:
+        # time.sleep(0.001)
         while ser.in_waiting:
-            recv += ser.read(1)
+            recv += ser.read_all()
+            # print(recv)
             # time.sleep(0.0005)
 
     # Check receive is REQ and send frame bytes array
@@ -45,25 +47,25 @@ while True:
         # Record time usage
         time_usage.append(perf_counter() - tmr)
         tmr = perf_counter()
-        print(f"{datetime.now()} Get board message: {recv.decode()}")
+        # print(f"{datetime.now()} Get board message: {recv.decode()}")
 
         # Select frame file by index
         if f_idx < range_list[1]:
             _f = file_list[0].all_frame[f_idx]
-        elif f_idx < range_list[2]:
-            _f = file_list[1].all_frame[f_idx - range_list[1]]
-        elif f_idx < range_list[3]:
-            _f = file_list[2].all_frame[f_idx - range_list[2]]
-        elif f_idx < range_list[4]:
-            _f = file_list[3].all_frame[f_idx - range_list[3]]
-        elif f_idx < range_list[5]:
-            _f = file_list[4].all_frame[f_idx - range_list[4]]
-        elif f_idx < range_list[6]:
-            _f = file_list[5].all_frame[f_idx - range_list[5]]
-        elif f_idx < range_list[7]:
-            _f = file_list[6].all_frame[f_idx - range_list[6]]
-        elif f_idx < range_list[8]:
-            _f = file_list[7].all_frame[f_idx - range_list[7]]
+        # elif f_idx < range_list[2]:
+        #     _f = file_list[1].all_frame[f_idx - range_list[1]]
+        # elif f_idx < range_list[3]:
+        #     _f = file_list[2].all_frame[f_idx - range_list[2]]
+        # elif f_idx < range_list[4]:
+        #     _f = file_list[3].all_frame[f_idx - range_list[3]]
+        # elif f_idx < range_list[5]:
+        #     _f = file_list[4].all_frame[f_idx - range_list[4]]
+        # elif f_idx < range_list[6]:
+        #     _f = file_list[5].all_frame[f_idx - range_list[5]]
+        # elif f_idx < range_list[7]:
+        #     _f = file_list[6].all_frame[f_idx - range_list[6]]
+        # elif f_idx < range_list[8]:
+        #     _f = file_list[7].all_frame[f_idx - range_list[7]]
         else:
             # Finish and break
             break
@@ -82,11 +84,11 @@ while True:
             ser.write(bytearray(_f))
 
         # Print out frame index number and start waiting next frame
-        print(f"{datetime.now()} Send frame: {f_idx}")
+        # print(f"{datetime.now()} Send frame: {f_idx}")
         f_idx += 1
 
 # Calculate FPS
-_total = sum(time_usage[1:])  # Skip first
-_avg_t = round(_total / (len(time_usage) - 2), 3)
+_total = sum(time_usage[2:])  # Skip first
+_avg_t = round(_total / len(time_usage[2:]), 3)
 print(f"Average time usage: {_avg_t}")
 print(f"Average FPS: {round(1 / _avg_t, 2)}")
--- README.md
+++ README.md
@@ -4,12 +4,15 @@ MCU Board coding practice and coding language performance comparison
 
 - ## TL; DR
 
-| Board | Code Lang. | LCD Mode | FPS  | Scan Mode |
-|:-----:|:----------:|:--------:|:----:|:-----:|
-| STM32F103 | C/C++(HAL) | SPI | 10.3 | Progressive |
-| ESP32 | Micropython | SPI | 3.x  | Progressive |
-| Pi Pico W | Micropython | Parallel | 4.22 | Progressive |
-| Pi Pico W | Micropython | Parallel | 8.13 | Interlaced |
+|   Board   | Code Lang.  | LCD Mode | Baud-rate |  Scan Mode  | Avg. FPS |
+|:---------:|:-----------:|:--------:|:---------:|:-----------:|:--------:|
+| STM32F103 | C/C++(HAL)  |   SPI    |  230400   | Progressive |   10.3   |
+| STM32H753 | C/C++(HAL)  |   SPI    |  460800   | Progressive |   13.5   |
+|   ESP32   | Micropython |   SPI    |  230400   | Progressive |   3.4    |
+| Pi Pico W | Micropython | Parallel |  230400   | Progressive |   4.22   |
+| Pi Pico W | Micropython | Parallel |  230400   | Interlaced  |   8.13   |
+   - Note: LCD pixels response time is not fast enough, it has terrible image sticking.
+   - The best display FPS is around ~10 FPS
 
 - ## Basic Concept:
    - Send frame bytes from PC via UART, then MCU board show result
